f07a824de076cb55580c82036839ad40b6deac21
TheJina <jainyakshittwo@live.com>
2020-06-23 19:57:30 +0530

    Add build folder

---
+++ build/precache-manifest.e08b87b068927ef6146a2fb6a6eb5a0b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254893a120a513553c89d69ac0792db0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4985a16534f41449c6a4",
+    "url": "/static/css/6.001f9a12.chunk.css"
+  },
+  {
+    "revision": "2c4953542c9f4661723b",
+    "url": "/static/js/0.989ff726.chunk.js"
+  },
+  {
+    "revision": "4b5a116f98c21ce22db9",
+    "url": "/static/js/1.7b34339f.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/static/js/1.7b34339f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6986a2c40e243b8d2a55",
+    "url": "/static/js/10.1930a3ec.chunk.js"
+  },
+  {
+    "revision": "6dc6d19bcba783483a60",
+    "url": "/static/js/11.de4adbd6.chunk.js"
+  },
+  {
+    "revision": "8d3635b19c264de23759",
+    "url": "/static/js/12.2d88cb08.chunk.js"
+  },
+  {
+    "revision": "30751a28eb9e594f5120",
+    "url": "/static/js/13.d42490c8.chunk.js"
+  },
+  {
+    "revision": "d4a39b9797252a897c67",
+    "url": "/static/js/2.875613c8.chunk.js"
+  },
+  {
+    "revision": "31097903bb548e219221",
+    "url": "/static/js/3.cf046604.chunk.js"
+  },
+  {
+    "revision": "4985a16534f41449c6a4",
+    "url": "/static/js/6.844f51da.chunk.js"
+  },
+  {
+    "revision": "3994c19536ae2b5b4b6b848729e56147",
+    "url": "/static/js/6.844f51da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e659217c511e71859b8",
+    "url": "/static/js/7.f71becc2.chunk.js"
+  },
+  {
+    "revision": "e77198dc8ad45b993ec5",
+    "url": "/static/js/8.9cb6d823.chunk.js"
+  },
+  {
+    "revision": "25d4d3e28000c69f8041",
+    "url": "/static/js/9.ccd7f155.chunk.js"
+  },
+  {
+    "revision": "eaaedf2dcf8067773e71",
+    "url": "/static/js/main.0fd94cab.chunk.js"
+  },
+  {
+    "revision": "76964fb01b91a6c6e41d",
+    "url": "/static/js/runtime-main.70d363e2.js"
+  }
+]);
